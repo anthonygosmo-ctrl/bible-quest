d6a9e8758b13fec1714d288570c42ad1dba396f1
Anthony Gosmo <anthonygosmo@email.com>
2025-12-06 00:49:02 +0845

    Fix base href for GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "9883985"
+    serviceWorkerVersion: "2202092775"
   }
 });
