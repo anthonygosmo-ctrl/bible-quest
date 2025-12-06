aa82ec7d3348a0a236650df1f2324cca00c571da
Anthony Gosmo <anthonygosmo@email.com>
2025-12-06 22:50:58 +0845

    update web

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3184704879"
+    serviceWorkerVersion: "3612034989"
   }
 });
